0d1b41fed48932dd0d253cdf96391ebd1fc36837
Kaushik Mishra <51756755+kaushik4u1@users.noreply.github.com>
2020-12-30 18:19:07 +0530

    Update 21-Type of Triangle.sql

--- SQL/Basic/21-Type of Triangle/21-Type of Triangle.sql
+++ SQL/Basic/21-Type of Triangle/21-Type of Triangle.sql
@@ -3,4 +3,5 @@ Enter your query here.
 */
 SELECT CASE
 WHEN A + B > C THEN CASE WHEN A = B AND B = C THEN 'Equilateral' WHEN A = B OR B = C OR A = C THEN 'Isosceles' WHEN A != B OR B != C OR A != C THEN 'Scalene' END
-ELSE 'Not A Triangle' END FROM TRIANGLES;
+ELSE 'Not A Triangle' END 
+FROM TRIANGLES;
